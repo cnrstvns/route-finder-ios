dc3fbdbfb245037b8fd23557544f752104a2cbc3
Connor Stevens <conmail1224@gmail.com>
2024-11-09 00:33:25 -0500

    Initial Commit

---
+++ RouteFinder/RouteFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteFinderApp.swift
+//  RouteFinder
+//
+//  Created by Connor Stevens on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RouteFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
