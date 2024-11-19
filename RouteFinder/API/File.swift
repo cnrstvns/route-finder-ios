a607529f8595ebee95a79f33b34283833f327d01
Connor Stevens <conmail1224@gmail.com>
2024-11-19 13:29:53 -0500

    Remove unreferenced files

--- RouteFinder/API/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  RouteFinder
-//
-//  Created by Connor Stevens on 11/19/24.
-//
-
-import Foundation
--- RouteFinder/Authentication.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Authentication.swift
-//  RouteFinder
-//
-//  Created by Connor Stevens on 11/18/24.
-//
-
-import Foundation
-
